3f5808a647007394109d1cb025e7016eb542a672
dermio <thisdjay@gmail.com>
2023-02-03 21:34:00 -0800

    Filter books displayed by genre

--- frontend/src/components/Books.js
+++ frontend/src/components/Books.js
@@ -1,9 +1,37 @@
-// import { useState } from "react"; // NEW
+import { useState, useEffect } from "react"; // NEW
 import { useQuery } from "@apollo/client";
 import { ALL_BOOKS } from "../queries";
 
 const Books = (props) => {
   const result = useQuery(ALL_BOOKS);
+  const [books, setBooks] = useState([]);
+  const [displayBooks, setDisplayBooks] = useState([]);
+  const [genreType, setGenreType] = useState("all genres");
+
+  useEffect(() => {
+    console.log("useEffect query GraphQL");
+
+    if (result.data) {
+      setBooks(result.data.allBooks);
+      setDisplayBooks(result.data.allBooks);
+    }
+  }, [result.data]);
+
+  useEffect(() => {
+    console.log("useEffect set genreType", genreType);
+
+    if (genreType === "all genres") {
+      const noFilterBooks = books;
+      console.log("All books, no genre filter", noFilterBooks);
+      setDisplayBooks(books);
+    }
+
+    if (genreType !== "all genres") {
+      const filteredBooks = books.filter((b) => b.genres.includes(genreType));
+      console.log("Filtered Books by genreType", filteredBooks);
+      setDisplayBooks(filteredBooks);
+    }
+  }, [genreType, books]);
 
   if (!props.show) {
     return null;
@@ -12,14 +40,22 @@ const Books = (props) => {
   if (result.loading) {
     return <div>loading...</div>;
   }
-  const books = result.data.allBooks; // Changed `books` to `allBooks`.
-  /* The author `name` field is part of the `author` object.
-  The `author` object is a field in the `allBooks` object. Edit line 33 to get
-  the author's name. */
+
+  console.log("[[[ books from query ]]]", books);
+
+  const genreButtons = books
+    .reduce((acc, book) => acc.concat(book.genres), [])
+    .filter((genre, i, arr) => arr.indexOf(genre) === i);
+
+  const filterGenre = (event) => {
+    setGenreType(event.target.getAttribute("data-genre"));
+  };
 
   return (
     <div>
       <h2>books</h2>
+      {genreType !== "all genres" &&
+        <p>in genre <span style={{fontWeight: "bold"}}>{genreType}</span></p>}
       <table>
         <tbody>
           <tr>
@@ -27,15 +63,29 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
+          {displayBooks.map((b) => (
+            <tr key={b.title} data-genre={b.genres}>
+              <td>{b.title}</td>
+              <td>{b.author.name}</td>
+              <td>{b.published}</td>
             </tr>
           ))}
         </tbody>
       </table>
+      <div>
+        {genreButtons.map((genre, i) => (
+          <button key={i} data-genre={genre} onClick={filterGenre}>
+            {genre}
+          </button>
+        ))}
+        <button
+          key={"all genres"}
+          data-genre={"all genres"}
+          onClick={filterGenre}
+        >
+          All genres
+        </button>
+      </div>
     </div>
   );
 };
